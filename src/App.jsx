b4a8fd7c856d015ae57bd948792993c7853058ad
raghavendra <raghavendra@rezolve.ai>
2025-11-21 17:00:27 +0530

    analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Companies from "./pages/companies";
 import CompanyDetails from "./pages/company-details";
+import { Analytics } from '@vercel/analytics/next';
 
 const ShootlensDashboard = () => {
     return (
@@ -8,6 +9,7 @@ const ShootlensDashboard = () => {
             <Routes>
                 <Route path="/" element={<Companies />} />
                 <Route path="/company/:symbol" element={<CompanyDetails />} />
+                  <Analytics />
             </Routes>
         </Router>
     )
